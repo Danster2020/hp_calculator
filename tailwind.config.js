23a1f7eb2949afffbb5f859209b1416249a0bae8
Danster2020 <daniel.bleckert@me.com>
2023-06-14 16:31:23 +0200

    font awesome added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark_back': '#0F171D',
+        'dark_surf': '#131D25',
+      },
+    },
   },
   plugins: [],
   safelist: [
